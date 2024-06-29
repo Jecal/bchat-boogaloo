db76489b5ee5c021b179be8c218aed6884afced0
Jullian <117240843+Jecal@users.noreply.github.com>
2024-06-29 17:03:37 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>bchat boogaloo deployment</p>
-      </header>
+      <nav className='navbar bg-body-tertiary'>
+        <div className='container'>
+          <a className='navbar-brand' href="#">bchat boogaloo</a>
+
+          <SignOut />
+        </div>
+      </nav>
     </div>
   );
 }
+
+// sign in component
+function SignIn() {
+  return (
+    <button className="btn btn-success">Sign In with Google</button>
+  );
+}
+
+// sign out component
+function SignOut() {
+  return (
+    <button className='btn btn-danger'>Sign Out</button>
+  )
+}
+
+// chat room component
+
+
+// chat message component
 
 export default App;
